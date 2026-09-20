ee40ea70cc9aa38c751805311eff855aaa553b32
GOWTHAM-MG-2006 <mggowtham2006@gmail.com>
2026-09-20 13:28:50 +0530

    strip(recommendation): remove OpenTelemetry instrumentation

--- src/recommendation/recommendation_server.py
+++ src/recommendation/recommendation_server.py
@@ -11,19 +11,9 @@ from concurrent import futures
 
 # Pip
 import grpc
-from opentelemetry import trace, metrics
-from opentelemetry._logs import set_logger_provider
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
-    OTLPLogExporter,
-)
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.resources import Resource
 
 from openfeature import api
 from openfeature.contrib.provider.flagd import FlagdProvider
-
-from openfeature.contrib.hook.opentelemetry import TracingHook
 
 # Local
 import logging
@@ -32,26 +22,17 @@ import demo_pb2_grpc
 from grpc_health.v1 import health_pb2
 from grpc_health.v1 import health_pb2_grpc
 
-from metrics import (
-    init_metrics
-)
-
 cached_ids = []
 first_run = True
 
 class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
     def ListRecommendations(self, request, context):
         prod_list = get_product_list(request.product_ids)
-        span = trace.get_current_span()
-        span.set_attribute("demo.product.recommended.count", len(prod_list))
         logger.info(f"Receive ListRecommendations for product ids:{prod_list}")
 
         # build and return response
         response = demo_pb2.ListRecommendationsResponse()
         response.product_ids.extend(prod_list)
-
-        # Collect metrics for this service
-        rec_svc_metrics["demo.recommendation.requests"].add(len(prod_list), {'recommendation.type': 'catalog'})
 
         return response
 
@@ -67,50 +48,40 @@ class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
 def get_product_list(request_product_ids):
     global first_run
     global cached_ids
-    with tracer.start_as_current_span("get_product_list") as span:
-        max_responses = 5
+    max_responses = 5
 
-        # Formulate the list of characters to list of strings
-        request_product_ids_str = ''.join(request_product_ids)
-        request_product_ids = request_product_ids_str.split(',')
+    # Formulate the list of characters to list of strings
+    request_product_ids_str = ''.join(request_product_ids)
+    request_product_ids = request_product_ids_str.split(',')
 
-        # Feature flag scenario - Cache Leak
-        if check_feature_flag("recommendationCacheFailure"):
-            span.set_attribute("demo.feature_flag.recommendation_cache", True)
-            if random.random() < 0.5 or first_run:
-                first_run = False
-                span.set_attribute("demo.recommendation.cache_hit", False)
-                logger.info("get_product_list: cache miss")
-                cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
-                response_ids = [x.id for x in cat_response.products]
-                cached_ids = cached_ids + response_ids
-                cached_ids = cached_ids + cached_ids[:len(cached_ids) // 4]
-                product_ids = cached_ids
-            else:
-                span.set_attribute("demo.recommendation.cache_hit", True)
-                logger.info("get_product_list: cache hit")
-                product_ids = cached_ids
-        else:
-            span.set_attribute("demo.feature_flag.recommendation_cache", False)
+    # Feature flag scenario - Cache Leak
+    if check_feature_flag("recommendationCacheFailure"):
+        if random.random() < 0.5 or first_run:
+            first_run = False
+            logger.info("get_product_list: cache miss")
             cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
-            product_ids = [x.id for x in cat_response.products]
+            response_ids = [x.id for x in cat_response.products]
+            cached_ids = cached_ids + response_ids
+            cached_ids = cached_ids + cached_ids[:len(cached_ids) // 4]
+            product_ids = cached_ids
+        else:
+            logger.info("get_product_list: cache hit")
+            product_ids = cached_ids
+    else:
+        cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
+        product_ids = [x.id for x in cat_response.products]
 
-        span.set_attribute("demo.product.count", len(product_ids))
+    # Create a filtered list of products excluding the products received as input
+    filtered_products = list(set(product_ids) - set(request_product_ids))
+    num_products = len(filtered_products)
+    num_return = min(max_responses, num_products)
 
-        # Create a filtered list of products excluding the products received as input
-        filtered_products = list(set(product_ids) - set(request_product_ids))
-        num_products = len(filtered_products)
-        span.set_attribute("demo.product.filtered.count", num_products)
-        num_return = min(max_responses, num_products)
+    # Sample list of indices to return
+    indices = random.sample(range(num_products), num_return)
+    # Fetch product ids from indices
+    prod_list = [filtered_products[i] for i in indices]
 
-        # Sample list of indices to return
-        indices = random.sample(range(num_products), num_return)
-        # Fetch product ids from indices
-        prod_list = [filtered_products[i] for i in indices]
-
-        span.set_attribute("demo.product.filtered.list", prod_list)
-
-        return prod_list
+    return prod_list
 
 
 def must_map_env(key: str):
@@ -127,27 +98,9 @@ def check_feature_flag(flag_name: str):
 
 
 if __name__ == "__main__":
-    service_name = must_map_env('OTEL_SERVICE_NAME')
-    api.set_provider(FlagdProvider(host=os.environ.get('FLAGD_HOST', 'flagd'), port=os.environ.get('FLAGD_PORT', 8013)))
-    api.add_hooks([TracingHook()])
-
-    # Initialize Traces and Metrics
-    tracer = trace.get_tracer_provider().get_tracer(service_name)
-    meter = metrics.get_meter_provider().get_meter(service_name)
-    rec_svc_metrics = init_metrics(meter)
-
-    # Initialize Logs
-    logger_provider = LoggerProvider(
-        resource = Resource.create({}),
-    )
-    set_logger_provider(logger_provider)
-    log_exporter = OTLPLogExporter(insecure=True)
-    logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
-    handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
-
-    # Attach OTLP handler to logger
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('main')
-    logger.addHandler(handler)
+    api.set_provider(FlagdProvider(host=os.environ.get('FLAGD_HOST', 'flagd'), port=os.environ.get('FLAGD_PORT', 8013)))
 
     catalog_addr = must_map_env('PRODUCT_CATALOG_ADDR')
     pc_channel = grpc.insecure_channel(catalog_addr)
